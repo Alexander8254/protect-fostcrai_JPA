3ec649c32546ccf3d902c61d16b3fbc1d57a0a18
Alexander8254 <110108934+Alexander8254@users.noreply.github.com>
2022-11-02 11:35:32 -0500

    .

--- appDemo/src/main/java/com/fostcraiv2/appDemo/service/ActividadService.java
+++ appDemo/src/main/java/com/fostcraiv2/appDemo/service/ActividadService.java
@@ -1,5 +1,12 @@
 package com.fostcraiv2.appDemo.service;
 
-public class ActividadService {
+import java.util.List;
 
+import com.fostcraiv2.appDemo.entidad.Actividad;
+
+public interface ActividadService {
+
+	public List<Actividad> listarTodasLasActividades();
+	
+	
 }
